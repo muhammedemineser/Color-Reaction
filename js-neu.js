55f86f19b00943b13cb123a124d435bf0f672797
muhammedemineser <ebotexo3@gmail.com>
2025-05-27 10:57:49 +0200

    Update js-neu.js

--- js-neu.js
+++ js-neu.js
@@ -423,7 +423,7 @@ starteCountdown(() => {
     buttonSec.classList.remove("sichtbar");
     anzeigeWrapper.classList.remove("sichtbar");
     document.getElementById("wuerfelAnimation").style.display = "flex";
-    document.getElementById("wuerfelAnimation").style.top = "70vh";
+    document.getElementById("wuerfelAnimation").style.top = "85%";
     document.getElementById("platzmacher").classList.add("sichtbar");
 document.getElementById("box-einschaetzung").textContent =
   "Selbsteinschätzung: " + anzahl;
