b8076c59121760a1218cf8d0d04e45982d8da443
muhammedemineser <ebotexo3@gmail.com>
2025-06-09 16:56:48 +0200

    Fix initial color after starting new round

--- js-neu.js
+++ js-neu.js
@@ -26,6 +26,19 @@ const countdownSound = new Audio("countdown.mp3");
 let counted = false;
 let countedNum = false;
 
+const farben = [
+  "blue",
+  "red",
+  "green",
+  "yellow",
+  "#FF10F0",
+  "#FF8000",
+  "#8B00FF",
+  "#8B4513",
+  "grey",
+  "white"
+];
+
 const bingBaseVolume = 0.2;
 const bingMaxVolume = 1;
 const wuerfelAnimContainer = document.getElementById("wuerfelAnimation");
@@ -488,7 +501,6 @@ function handleGameInput() {
     }
 
     zustand = (zustand % 10) + 1;
-    const farben = ["blue", "red", "green", "yellow", "#FF10F0", "#FF8000", "#8B00FF", "#8B4513", "grey", "white"];
     karte.style.backgroundColor = farben[zustand - 1];
     input.value = "";
     let jetzt = performance.now();
@@ -637,6 +649,7 @@ starteCountdown(() => {
     anzeigeWrapper.classList.add("sichtbar");
     anzeigeWrapper.style.display = "flex";
     karte.classList.add("sichtbar");
+    karte.style.backgroundColor = farben[0];
   });
 
 
@@ -953,6 +966,7 @@ nextRoundBtn.addEventListener("click", () => {
   timerGestartet = false;
   countedNum = false;
   counted = false;
+  karte.style.backgroundColor = farben[0];
   zustand = 0;
   korrektAnzahl = 0;
   reaktionszeiten = [];
